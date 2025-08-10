a0a678dd3e36da10bef1110e775655ad39fa8aee
hparra07 <howard.parra95@gmail.com>
2025-08-10 10:53:43 -0300

    subiendo deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pods-by-lumen-preview-site/', // 👈 reemplazalo
+  base: './', 
   plugins: [react()],
 })
